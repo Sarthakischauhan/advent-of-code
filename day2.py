637d05e0df15dc4088566c0e2752b548b9458c36
Sarthakischauhan <sarthakchauhann@gmail.com>
2024-12-06 17:20:49 -0800

    Day2 part 2 solved

--- day2.py
+++ day2.py
@@ -35,4 +35,53 @@ def part1():
         count += 1 if (safe and (inc or dec)) else 0
     print(count)
 
-part1()
+def part2():
+    with open("puzzle2.txt", "r") as file:
+        reports = file.readlines()
+    file.close()
+
+    count = 0
+
+    for report in reports:
+        report = report.strip().split(" ")
+        report = list(map(int, report))
+
+        prev = report[0]
+        inc = None  
+        safe = True
+        crs = 0
+        for i, level in enumerate(report[1:], start=1):
+            diff = level - prev
+
+            if 1 <= abs(diff) <= 3:  
+                if diff > 0:  
+                    if inc is None:  
+                        inc = True
+                    elif not inc:  
+                        if crs < 1:
+                            crs += 1
+                        else:
+                            safe = False
+                            break
+                elif diff < 0:  
+                    if inc is None:
+                        inc = False
+                    elif inc:  
+                        if crs < 1:
+                            crs += 1
+                        else:
+                            safe = False
+                            break
+            else:  
+                if crs < 1:  
+                    crs += 1
+                else:
+                    safe = False
+                    break
+
+            prev = level
+
+        count += 1 if safe else 0
+
+    print(count)
+part2()
